bf463c19430fb9c54c98ca03f63a94d25886014d
amydiab <amydiab30@gmail.com>
2026-04-28 20:37:54 +0000

    fix: harmonisation total offres

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def load_data():
     df = pd.read_csv('dataset_final_propre.csv', encoding='utf-8')
     df['mois'] = pd.to_datetime(df['mois'], errors='coerce')
     df = df.dropna(subset=['mois'])
-    df = df[df['entreprise'] != 'Non spécifié']
+    df = df[~df['entreprise'].str.contains('Non', na=False)]
     return df
 
 df = load_data()
